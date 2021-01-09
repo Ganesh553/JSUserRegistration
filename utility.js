d4a350b47a04de0903367a8ceb684e6cfa088468
unknown <Ganesh553>
2021-01-09 09:48:20 +0530

    Phone Number Validation

---
+++ utility.js
@@ -0,0 +1,47 @@
+class Utility{
+    checkFirstName(name){
+        var regex = /^[A-Z]{1,}[a-z]{2,}/;
+        if(regex.test(name)){
+            console.log("True :",name);
+            return name;
+        }
+        else{
+            console.log("False :",name);
+            return name;
+        }
+    }
+    checkLastName(name){
+        var regex = /^[A-Z]{1,}[a-z]{2,}/;
+        if(regex.test(name)){
+            console.log("True :",name);
+            return name;
+        }
+        else{
+            console.log("False :",name);
+            return name;
+        }
+    }
+    checkEmailId(mail){
+        var regex = /^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if(regex.test(mail)){
+            console.log("True :",mail);
+            return mail;
+        }
+        else{
+            console.log("False :",mail);
+            return mail;
+        }
+    }
+    checkPhoneNumber(number){
+        var regex = /^([0-9]{2})+ +([789]{1})+([0-9]{9})+$/;
+        if(regex.test(number)){
+            console.log("True :",number);
+            return number;
+        }
+        else{
+            console.log("False :",number);
+            return number;
+        }
+    }
+}
+module.exports = new Utility();
