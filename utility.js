7d680e3d28a923bf82aedf6a9ca77dcc31cc83e8
Ganesh553 <72643633+Ganesh553@users.noreply.github.com>
2021-01-10 19:16:32 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -1,36 +1,37 @@
-class Utility{
-    checkFirstName(name){
-        var regex = /^[A-Z]{1,}[a-z]{2,}/;
-        if(regex.test(name)){
-            console.log("True :",name);
-            return name;
-        }
-        else{
-            console.log("False :",name);
-            return name;
-        }
-    }
-    checkLastName(name){
-        var regex = /^[A-Z]{1,}[a-z]{2,}/;
-        if(regex.test(name)){
-            console.log("True :",name);
-            return name;
-        }
-        else{
-            console.log("False :",name);
-            return name;
-        }
-    }
+const readLine = require('readLine-sync')
 
-    checkEmailId(mail){
-        var regex = /^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        if(regex.test(mail)){
-            console.log("True :",mail);
-            return mail;
+class Utility{
+    checkFirstName(){
+        var flag=true;
+        var regex = /^[A-Z]{1,}[a-z]{2,}/;
+        while(flag){
+            var FirstName = readLine.question("Enter the First Name: ")
+            if(regex.test(FirstName))
+                return;
+            else
+                flag=true;
         }
-        else{
-            console.log("False :",mail);
-            return mail;
+    }
+    checkLastName(){
+        var flag=true;
+        var regex = /^[A-Z]{1,}[a-z]{2,}/;
+        while(flag){
+            var LastName = readLine.question("Enter the Last Name: ")
+            if(regex.test(LastName))
+                return;
+            else
+                flag=true;
+        }
+    }
+    checkEmailId(){
+        var flag=true;
+        var regex = /^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        while(flag){
+            let Email = readLine.question("Enter the Email_Id: ")
+            if(regex.test(Email))
+                return;
+            else
+                flag=true;
         }
     }
 }
